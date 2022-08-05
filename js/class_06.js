6cf77eda63188cd3f3d773f6a8ae85a23862afd2
ArthurEstevan <arthur.estevan01@gmail.com>
2022-08-04 21:32:11 -0300

    Learning Ajax

---
+++ js/class_06.js
@@ -0,0 +1,92 @@
+// $.ajax()
+
+// $.ajax(
+//    {
+
+//    }
+// )
+
+$("#meu-ip").on("click", () => {
+
+    $.ajax({
+        url: "https://httpbin.org/ip",
+        type: "get",
+        success: (retorno) => {
+            console.log(`Funcionando`, retorno);
+        },
+        error: (motivo) => {
+            console.warn(`Error`, motivo);
+        },
+    });
+});
+
+$("#movie").on("click", () => {
+
+    $.ajax({
+        url: "https://httpbin.org/get?nome=rubem&aula=javascript%20com%20ajax",
+        type: "get",
+        success: (retorno) => {
+            console.log(`Funcionando`, retorno.args);
+        },
+        error: (motivo) => {
+            console.warn(`Error`, motivo);
+        },
+    });
+});
+
+$("#delay").on("click", () => {
+    // console.log("Normal", encodeURI("https://httpbin.org/delay/10")),
+    // console.log("Normal", encodeURI("https://httpbin.org/delay/10"));
+    
+    // "https://httpbin.org/get?nome=Arthur&aula=javascript%20com%20ajax"
+    // acima consigo passar váriaveis após ?
+
+    let number = $("#tempo").val()
+
+    $.ajax({
+        type: "get",
+        url: encodeURI("https://httpbin.org/delay/"+number),
+        success: (retorno) => {
+            console.log(`Funcionando`, retorno);
+        },
+        error: (motivo) => {
+            console.warn(`Error`, motivo);
+        },
+    });
+});
+
+
+$("#post").on("click", () => {
+
+
+    var objeto = {
+        
+        nome: "Arthur",
+        email: "arthur.estevan01@gmail.com",
+
+    };
+
+
+    $.ajax( {
+        url: encodeURI("https://httpbin.org/post"),
+        type: "post",
+
+        header: {
+
+           'Accept': "aplication/json",
+            'Content-type': "aplication/json"
+        },
+
+        data: JSON.stringify(objeto),
+
+        success: (dados) => {
+            console.log("Funciona", dados);
+        },
+
+        error: (erro) => {
+            console.log("Error", erro);
+        }
+
+    })
+
+})
